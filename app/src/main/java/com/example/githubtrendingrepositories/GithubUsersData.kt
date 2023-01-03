cc703e9c2100ae5baf6c751128e849d6369910c8
Atif-09 <ma2669637@gmail.com>
2023-01-03 18:56:21 +0500

    Added Retrofit

--- app/src/main/java/com/example/githubtrendingrepositories/GithubUsersData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.githubtrendingrepositories
-
-data class GithubUsersData(
-    val incomplete_results: Boolean,
-    val items: List<Item>,
-    val total_count: Int
-)
--- app/src/main/java/com/example/githubtrendingrepositories/MainActivity.kt
+++ app/src/main/java/com/example/githubtrendingrepositories/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.githubtrendingrepositories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.githubtrendingrepositories.api.ApiInterface
+import com.example.githubtrendingrepositories.api.ApiUtilities
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val usersApi = ApiUtilities.getInstance().create(ApiInterface::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val result = usersApi.getUsers()
+
+            if (result.body() != null){
+
+
+                Log.d("Atif", "Oncreate ${result.body()}")
+                }
+        }
+
+
     }
 }
